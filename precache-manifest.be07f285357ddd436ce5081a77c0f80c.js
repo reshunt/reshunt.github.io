40692ac72a673687534cb40a52805669b617adfa
xm <xiaxianmao@thefifthera.com>
2020-02-07 12:06:14 +0800

    deploy

---
+++ precache-manifest.be07f285357ddd436ce5081a77c0f80c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a01aa712de6613f9902b025c1285d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e70fa9e5cc5054d9ca",
+    "url": "/static/css/2.cab916c1.chunk.css"
+  },
+  {
+    "revision": "a2f8965e237bc83df6c3",
+    "url": "/static/css/main.16f0543c.chunk.css"
+  },
+  {
+    "revision": "20e70fa9e5cc5054d9ca",
+    "url": "/static/js/2.27357c84.chunk.js"
+  },
+  {
+    "revision": "a2f8965e237bc83df6c3",
+    "url": "/static/js/main.7ca23266.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
